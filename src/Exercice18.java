6d6e2347d8153bd6b7414d84511ec5f26b840197
rmh <alumar1999@ieselcaminas.org>
2022-09-22 19:13:36 +0200

    Second commit

--- src/Exercice18.java
+++ src/Exercice18.java
@@ -1,66 +1,35 @@
 import java.util.Scanner;
 
-public class Exercice18 {
+public class Exercice18bis {
 
     public static void main(String[] args) {
-        double h, hourm, money, net, gross, tax;
+        double hour, price, gross, taxes, net;
 
         Scanner inputValue;
-        inputValue= new Scanner(System.in);
+        inputValue = new Scanner(System.in);
 
         System.out.println("Enter hours: ");
-        h= inputValue.nextDouble();
-        System.out.println("Enter $/hour: ");
-        hourm= inputValue.nextInt();
+        hour= inputValue.nextDouble();
+        System.out.println("Enter hour/price: ");
+        price = inputValue.nextDouble();
 
-        if (h < 35) {
-            money = h * hourm;
-            if ( money < 500 ) {
-                net=money;
-                gross=h*hourm;
-                tax=0;
-                System.out.println("Net salary: " + net);
-                System.out.println("Gross salary: " + gross);
-                System.out.println("Taxes: " + tax);
-            } else if ( money < 900) {
-                tax= 0.25 * (money - 500);
-                gross=h*hourm;
-                net=money-tax;
-                System.out.println("Net salary: " + net);
-                System.out.println("Gross salary: " + gross);
-                System.out.println("Taxes: " + tax);
-            } else {
-                tax= 0.45 * (money - 900)+0.25*400;
-                gross=h*hourm;
-                net=money-tax;
-                System.out.println("Net salary: " + net);
-                System.out.println("Gross salary: " + gross);
-                System.out.println("Taxes: " + tax);
-            }
+        if (hour > 35) {
+            gross = 35 * price + ( hour - 35 ) * 1.5 * price;
         } else {
-            money = 35 * hourm + (h-35)*1.5*hourm;
-            if ( money < 500 ) {
-                net=money;
-                gross=35*hourm + 1.5*hourm*(h-35);
-                tax=0;
-                System.out.println("Net salary: " + net);
-                System.out.println("Gross salary: " + gross);
-                System.out.println("Taxes: " + tax);
-            } else if ( money < 900) {
-                tax= 0.25 * (money - 500);
-                gross=35*hourm + 1.5*hourm*(h-35);
-                net=money-tax;
-                System.out.println("Net salary: " + net);
-                System.out.println("Gross salary: " + gross);
-                System.out.println("Taxes: " + tax);
-            } else {
-                tax= 0.45 * (money - 900)+ 0.25*400;
-                gross=35*hourm + 1.5*hourm*(h-35);
-                net=money-tax;
-                System.out.println("Net salary: " + net);
-                System.out.println("Gross salary: " + gross);
-                System.out.println("Taxes: " + tax);
+            gross = hour * price;
         }
+        if (gross > 900) {
+            taxes = (gross - 900) * 0.45 + 400 * 0.25;
+            net = gross - taxes;
+        } else if (gross > 500) {
+            taxes = (gross - 500) * 0.25;
+            net = gross - taxes;
+        } else {
+            taxes = 0;
+            net = gross;
+        }
+        System.out.println("Gross: " + gross);
+        System.out.println("Net: " + net);
+        System.out.println("Taxes: " + taxes);
     }
-}
 }
